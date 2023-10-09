0df606ec5e2066c908ba6a1ca516aef972d07639
JeffersonLuckianto <106744794+JeffersonLuckianto@users.noreply.github.com>
2023-10-09 09:03:56 +0700

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Home')
+@section('content')
+        <h1>Selamat Datang Jefferson Luckianto</h1>
+        <a href="Selamatdatang">Ke halaman welcome</a>
+@endsection
